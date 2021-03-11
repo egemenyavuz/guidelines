ef941c635388db7f6aa34d57c6d4ffb0dbcd2089
Melih Egemen Yavuz <egemen.yavuz@soprasteria.com>
2021-03-11 10:15:14 +0100

    Update git_action.sh

--- git_action.sh
+++ git_action.sh
@@ -10,29 +10,29 @@ set -e
 _REPO="${GITHUB_REPO}"
 _DEFAULT_REPO_NAME="${_REPO/#*\//}"
 _REPO_NAME="${DOCKER_REPO_NAME:-$_DEFAULT_REPO_NAME}"
-_DOCKER_ID_FORPUSH="${DOCKER_ORGNAME:-$DOCKER_USERNAME}"
+_DOCKER_ID="${DOCKER_ORGNAME:-$DOCKER_USERNAME}"
 
 _DOCKER_REPO_TAG="development"
 if  [ "${GITHUB_REF:0:10}" = "refs/tags/" ]
 then
   _DOCKER_REPO_TAG="${GITHUB_REF:10}"
 fi
-echo "Docker image is set to'${_DOCKER_ID_FORPUSH}/${_REPO_NAME}:${_DOCKER_REPO_TAG}'"
+echo "Docker image is set to'${_DOCKER_ID}/${_REPO_NAME}:${_DOCKER_REPO_TAG}'"
 
 
 #build docker image
-docker image build --label Commit="${GITHUB_SHA}" --label BuildNumber="${GITHUB_RUN_NUMBER}" --label RepoSlug="${_REPO}" -t ${_REPO_NAME}:${_DOCKER_REPO_TAG} .
+docker build --label Commit="${GITHUB_SHA}" --label BuildNumber="${GITHUB_RUN_NUMBER}" --label RepoSlug="${_REPO}" -t ${_REPO_NAME}:${_DOCKER_REPO_TAG} .
 
 #push to dockerhub if tagged or pushed to master
 if  [ "${GITHUB_REF:0:10}" = "refs/tags/" ] || [ "${GITHUB_REF}" = "refs/heads/master" ]
 then
-  if [ -z "${_DOCKER_ID_FORPUSH}" ]
+  if [ -z "${_DOCKER_ID}" ]
   then
     echo "Neither DOCKER_ORGNAME nor DOCKER_USERNAME is set.Cannot push to docker registry!"
     exit 1
   fi
-  docker image tag ${_REPO_NAME}:${_DOCKER_REPO_TAG} ${_DOCKER_ID_FORPUSH}/${_REPO_NAME}:${_DOCKER_REPO_TAG}
-  docker image push ${_DOCKER_ID_FORPUSH}/${_REPO_NAME}:${_DOCKER_REPO_TAG}
+  docker tag ${_REPO_NAME}:${_DOCKER_REPO_TAG} ${_DOCKER_ID}/${_REPO_NAME}:${_DOCKER_REPO_TAG}
+  docker push ${_DOCKER_ID}/${_REPO_NAME}:${_DOCKER_REPO_TAG}
   docker logout
 else
   echo "Skipping docker-push as per the logic"
